0537f37578e076fea26c17fc23e4b6a172a40222
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-05 15:55:12 +0300

    Initial Commit

---
+++ Remembrall/RemembrallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemembrallApp.swift
+//  Remembrall
+//
+//  Created by Dmitry Sharabin on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RemembrallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
